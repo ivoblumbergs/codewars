77593bef2c0ab135341f607af6572fd68997966d
ivoblumbergs <ivo.blumbergs@gmail.com>
2023-06-21 09:26:49 +0100

    added a solution for codewars challange

---
+++ calculateAvarage.js
@@ -0,0 +1,17 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+function findAverage(array) {
+    if (array.length === 0) {
+      return 0;
+  }
+    
+    
+    let sum = 0;
+    for(let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    const avarage = sum / array.length;
+    return avarage;
+  }
