fa265c96ca94b7b81e64d8aef4963b38d39de2f3
ivoblumbergs <ivo.blumbergs@gmail.com>
2023-07-02 09:04:24 +0100

    added solution for codewars challange

---
+++ noBoringZeros.js
@@ -0,0 +1,11 @@
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+
+function noBoringZeros(n) {
+    while(n%10==0 && n!=0){n/=10;}
+    return n;
+  }
