f92c4c950e93f9b0e398deec6f5f9867ba1200c4
ivoblumbergs <ivo.blumbergs@gmail.com>
2023-07-15 10:50:09 +0100

    added solution for codewars challange

---
+++ sum_of_differences.js
@@ -0,0 +1,5 @@
+// our task is to sum the differences between consecutive pairs in the array in descending order.
+
+function sumOfDifferences(arr) {
+    return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
+}
